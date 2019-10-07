60d604acc0fa6075244fb308d8f133dd2163e704
alexey100196 <alexeysobolev071@gmail.com>
2019-10-07 14:33:58 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <component :is="layout">
+      <router-view />
+    </component>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  // import EmptyLayout from '@/Layouts/EmptyLayout'
+  export default {
+    computed: {
+      layout() {
+        console.log(this.$route.meta)
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  @import '~materialize-css/dist/css/materialize.css';
+  @import 'assets/index.css';
+</style>
---
+++ src/router.js
@@ -0,0 +1,29 @@
+ import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/Login',
+      name: 'login',
+      meta: {layout: 'empty'},
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/Categories',
+      name: 'categories',
+      meta: {layout: 'main'},
+      component: () => import('./views/Categories.vue')
+    },
+  ]
+})
